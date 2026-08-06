e50bbd4f244a4fe4cfb32055ba833f2d89f43dfa
Shiva-shankar1 <192472271.simats@saveetha.com>
2026-08-06 11:54:11 +0530

    Delete sentence_tokenization.py

--- sentence_tokenization.py
+++
@@ -1,7 +0,0 @@
-import re
-text = """Artificial Intelligence is transforming the world.
-It is used in healthcare, agriculture, and education.
-Python makes AI development easier! Do you like AI? Yes, it is powerful."""
-sentences = re.split(r'(?<=[.!?])\s+', text)
-for i, sentence in enumerate(sentences, 1):
-    print(f"{i}. {sentence}")
